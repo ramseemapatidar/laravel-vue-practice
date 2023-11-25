ca20ee8768e41587b3c3cdd0aecdcf31c0fe97cc
Ankur <ankur@Ankurs-MacBook-Air.local>
2023-11-25 12:11:37 +0530

    fifth commit user form layout in modal

--- resources/js/pages/users/UserList.vue
+++ resources/js/pages/users/UserList.vue
@@ -36,36 +36,81 @@ onMounted(() => {
 
     <div class="content">
         <div class="container-fluid">
-            <!-- <div class="row"> -->
-                <div class="card">
-                    <div class="card-body">
-                        <table class="table">
-                            <thead>
-                                <tr>
-                                    <th style="width:10px">#</th>
-                                    <th>Name</th>
-                                    <th>Email</th>
-                                    <th>Register Date</th>
-                                    <th>Role</th>
-                                    <th>Action</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr v-for="(user,index) in users" :key="user.id" >
-                                    <td>{{ index+1 }}</td>
-                                    <td>{{ user.name }}</td>
-                                    <td>{{ user.email }}</td>
-                                    <td>-</td>
-                                    <td>-</td>
-                                    <td>-</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
+            <button type="button" class="btn btn-primary mb-2" data-toggle="modal" data-target="#createUserModal">Add New User</button>
+            <div class="card">
+                <div class="card-body">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th style="width:10px">#</th>
+                                <th>Name</th>
+                                <th>Email</th>
+                                <th>Register Date</th>
+                                <th>Role</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr v-for="(user,index) in users" :key="user.id" >
+                                <td>{{ index+1 }}</td>
+                                <td>{{ user.name }}</td>
+                                <td>{{ user.email }}</td>
+                                <td>-</td>
+                                <td>-</td>
+                                <td>-</td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
-
-            <!-- </div> -->
-
+            </div>
         </div>
     </div>
+
+    <!-- Add user Modal -->
+    <div class="modal fade" id="createUserModal" data-backdrop="static" tabindex="-1" role="dialog"
+        aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="staticBackdropLabel">
+                        <span>Add New User</span>
+                    </h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <form ref="form">
+                    <div class="modal-body">
+                        <div class="form-group">
+                            <label for="name">Name</label>
+                            <input name="name" type="text" class="form-control"
+                                id="name" aria-describedby="nameHelp" placeholder="Enter full name" />
+                            <span class="invalid-feedback"></span>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="email">Email</label>
+                            <input name="email" type="email" class="form-control "
+                                 id="email" aria-describedby="nameHelp"
+                                placeholder="Enter full name" />
+                            <span class="invalid-feedback"></span>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="email">Password</label>
+                            <input name="password" type="password" class="form-control "
+                                 id="password" aria-describedby="nameHelp"
+                                placeholder="Enter password" />
+                            <span class="invalid-feedback"></span>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                        <button type="submit" class="btn btn-primary">Save</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    <!-- End user Modal -->
 </template>
