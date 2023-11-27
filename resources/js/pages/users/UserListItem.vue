59d5d3d7f27f0e5782410ed5695ad8f0edc0878e
Ankur <ankur@Ankurs-MacBook-Air.local>
2023-11-27 18:03:34 +0530

    seventeen commit role editign

--- resources/js/pages/users/UserListItem.vue
+++ resources/js/pages/users/UserListItem.vue
@@ -7,6 +7,18 @@ import { useToastr } from '../../toastr.js';
 const userIdBeingDeleted = ref(null);
 const emit = defineEmits(['userDeleted','editUser'])
 const toastr = useToastr();
+
+const roles = ref([
+    {
+        name: 'Admin',
+        value: 1
+    },
+    {
+        name: 'User',
+        value: 2,
+    }
+]);
+
  defineProps({
     user: Object,
     index: Number,
@@ -31,6 +43,15 @@ const deleteUser = () => {
 const editUser = (user) =>{
     emit('editUser',user);
 }
+
+const changeRole = (user, role) => {
+    axios.patch(`/api/users/${user.id}/change-role`, {
+        role: role,
+    })
+    .then(() => {
+        toastr.success('Role changed successfully!');
+    })
+};
 </script>
 <template>
     <tr >
@@ -38,7 +59,12 @@ const editUser = (user) =>{
         <td>{{ user.name }}</td>
         <td>{{ user.email }}</td>
         <td>{{ formatDate(user.created_at) }}</td>
-        <td>{{ user.role }}</td>
+        <td>
+            <select  class="form-control" @change="changeRole(user, $event.target.value)">
+                <option v-for="role in roles" :value="role.value" :selected="(user.role === role.name)">{{ role.name }}</option>
+
+            </select>
+        </td>
         <td>
             <a href="#" @click.prevent="editUser(user)"><i class="fa fa-edit"></i></a>
             <a href="#" @click.prevent="confirmationModal(user)"><i class="fa fa-trash text-danger ml-2"></i></a>
