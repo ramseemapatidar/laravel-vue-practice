d91b712e352e54faa2a4110e2214771b64e97e97
Ankur <ankur@Ankurs-MacBook-Air.local>
2023-11-27 15:37:30 +0530

    sixteen commit code organize list and delete case

--- resources/js/pages/users/UserList.vue
+++ resources/js/pages/users/UserList.vue
@@ -4,13 +4,14 @@ import { ref,  onMounted, reactive } from 'vue';
 import { Form, Field } from 'vee-validate';
 import * as yup from 'yup';
 import { useToastr } from '../../toastr.js';
-import { formatDate } from '../../helper.js';
+
+import UserListItem from './UserListItem.vue';
 const users = ref([]);
 const editing = ref(false);
 const formValues = ref();
 const form = ref(null);
 const toastr = useToastr();
-const userIdBeingDeleted = ref(null);
+
 const getUsers = () => {
 
     axios.get('/api/users')
@@ -87,19 +88,9 @@ const handleSubmit = (values, actions) => {
     }
 }
 
-const confirmationModal = (user) =>{
-    userIdBeingDeleted.value = user.id;
-    $('#deleteUserModal').modal('show');
+const userDeleted = (userId) => {
+    users.value = users.value.filter(user => user.id !== userId);
 }
-
-const deleteUser = () => {
-    axios.delete(`/api/users/${userIdBeingDeleted.value}`)
-    .then(() => {
-        $('#deleteUserModal').modal('hide');
-        toastr.success('User deleted successfully!');
-        users.value.data = users.value.data.filter(user => user.id !== userIdBeingDeleted.value);
-    });
-};
 
 onMounted(() => {
     getUsers()
@@ -141,17 +132,7 @@ onMounted(() => {
                             </tr>
                         </thead>
                         <tbody>
-                            <tr v-for="(user,index) in users" :key="user.id" >
-                                <td>{{ index+1 }}</td>
-                                <td>{{ user.name }}</td>
-                                <td>{{ user.email }}</td>
-                                <td>{{ formatDate(user.created_at) }}</td>
-                                <td>{{ user.role }}</td>
-                                <td>
-                                    <a href="#" @click.prevent="editUser(user)"><i class="fa fa-edit"></i></a>
-                                    <a href="#" @click.prevent="confirmationModal(user)"><i class="fa fa-trash text-danger ml-2"></i></a>
-                                </td>
-                            </tr>
+                            <UserListItem  v-for="(user,index) in users" :key="user.id" :user="user" :index="index" @user-deleted="userDeleted"></UserListItem>
                         </tbody>
                     </table>
                 </div>
@@ -206,28 +187,5 @@ onMounted(() => {
     </div>
     <!-- End user Modal -->
 
-    <!-- Start confirmation Modal -->
-    <div class="modal fade" id="deleteUserModal" data-backdrop="static" tabindex="-1" role="dialog"
-        aria-labelledby="staticBackdropLabel" aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="staticBackdropLabel">
-                        <span>Delete User</span>
-                    </h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <h5>Are you sure you want to delete this user ?</h5>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
-                    <button @click.prevent="deleteUser" type="button" class="btn btn-primary">Delete User</button>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- End confirmation Modal -->
+
 </template>
