25e25e13955adab926134b1b1bc6f1bfd65dd4a8
Ankur <ankur@Ankurs-MacBook-Air.local>
2023-11-29 11:29:55 +0530

    twenty two commit appointment layout and create model factory and migartion useing this command php artisan make:model Modelname -mf

--- resources/js/pages/appointments/ListAppointments.vue
+++ resources/js/pages/appointments/ListAppointments.vue
@@ -3,12 +3,12 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h1 class="m-0">Appointment</h1>
+                    <h1 class="m-0">Appointments</h1>
                 </div>
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
                         <li class="breadcrumb-item"><a href="#">Home</a></li>
-                        <li class="breadcrumb-item active">Appointment</li>
+                        <li class="breadcrumb-item active">Appointments</li>
                     </ol>
                 </div>
             </div>
@@ -19,10 +19,63 @@
     <div class="content">
         <div class="container-fluid">
             <div class="row">
+                <div class="col-lg-12">
+                    <div class="d-flex justify-content-between mb-2">
+                        <div>
+                            <router-link to="/admin/appointments/create">
+                                <button class="btn btn-primary"><i class="fa fa-plus-circle mr-1"></i> Add New
+                                    Appointment</button>
+                            </router-link>
+                        </div>
+                        <div class="btn-group">
+                            <button  type="button" class="btn">
+                                <span class="mr-1">All</span>
+                                <span class="badge badge-pill badge-info"></span>
+                            </button>
 
+                            <button type="button"
+                                class="btn" >
+                                <span class="mr-1">-</span>
+                                <span class="badge badge-pill">-</span>
+                            </button>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="card-body">
+                            <table class="table table-bordered">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">#</th>
+                                        <th scope="col">Client Name</th>
+                                        <th scope="col">Date</th>
+                                        <th scope="col">Time</th>
+                                        <th scope="col">Status</th>
+                                        <th scope="col">Options</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>-</td>
+                                        <td>-</td>
+                                        <td>-</td>
+                                        <td>-</td>
+                                        <td>
+                                            <span class="badge" ></span>
+                                        </td>
+                                        <td>
+                                            <a href=""> <i class="fa fa-edit mr-2"></i></a>
 
+                                            <a href="#">
+                                                <i class="fa fa-trash text-danger"></i>
+                                            </a>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
             </div>
-
         </div>
     </div>
 </template>
