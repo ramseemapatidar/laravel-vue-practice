b114d273681e5437eb381f14fdbc7db605869145
Ankur <ankur@Ankurs-MacBook-Air.local>
2023-12-02 12:16:44 +0530

    twenty five create appointment form

---
+++ resources/js/pages/appointments/AppointmentForm.vue
@@ -0,0 +1,84 @@
+<script setup>
+
+</script>
+<template>
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0">
+                        Create Appointment</h1>
+                </div>
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item">
+                            <router-link to="/admin/dashboard">Home</router-link>
+                        </li>
+                        <li class="breadcrumb-item">
+                            <router-link to="/admin/appointments">Appointments</router-link>
+                        </li>
+                        <li class="breadcrumb-item active">
+                            Create
+                        </li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <form>
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label for="title">Title</label>
+                                            <input type="text" class="form-control" id="title" placeholder="Enter Title">
+                                            <span class="invalid-feedback"></span>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label for="client">Client Name</label>
+                                            <select id="client" class="form-control">
+                                                <option value="">select</option>
+                                            </select>
+                                            <span class="invalid-feedback"></span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label for="start-time">Start Time</label>
+                                            <input type="text" class="form-control flatpickr"  id="start-time">
+                                            <span class="invalid-feedback"></span>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label for="end-time">End Time</label>
+                                            <input type="text" class="form-control flatpickr" id="end-time">
+                                            <span class="invalid-feedback"></span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label for="description">Description</label>
+                                    <textarea class="form-control" id="description" rows="3"
+                                        placeholder="Enter Description"></textarea>
+                                    <span class="invalid-feedback"></span>
+                                </div>
+                                <button type="submit" class="btn btn-primary">Submit</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
