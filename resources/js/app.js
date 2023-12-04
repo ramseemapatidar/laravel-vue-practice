1ae10d2b4e14a839835ab3713787982876a863e4
Ankur <ankur@Ankurs-MacBook-Air.local>
2023-12-04 12:35:43 +0530

    create login page in vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -10,7 +10,7 @@ import { createApp } from 'vue/dist/vue.esm-bundler.js';
 import { createRouter, createWebHistory } from 'vue-router';
 
 import Routes from './routes.js';
-
+import Login from './pages/auth/Login.vue';
 const app = createApp({});
 
 
@@ -21,5 +21,5 @@ const router = createRouter({
 
 
 app.use(router);
-
+app.component('Login',Login)
 app.mount('#app');
